e3c88bd68485a7471a74aa3ee52b136055f15ab2
WM <lucek.books@gmail.com>
2026-05-19 12:38:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
+// app.js
+
+// KOSZYK
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function add(name, price) {
-  cart.push({name, price});
-  localStorage.setItem("cart", JSON.stringify(cart));
-  updateCount();
-}
-
-function updateCount() {
+function updateCart(){
   document.getElementById("cartCount").innerText = cart.length;
 }
 
-updateCount();
+function addToCart(name, price){
+
+  cart.push({
+    name,
+    price
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  updateCart();
+
+  alert(name + " dodano do koszyka");
+}
+
+updateCart();
+
+
+// SLIDER
+
+let slides = [
+  "MainPage_1.jpg",
+  "MainPage_2.jpg"
+];
+
+let current = 0;
+
+setInterval(() => {
+
+  current++;
+
+  if(current >= slides.length){
+    current = 0;
+  }
+
+  document.getElementById("slider").src = slides[current];
+
+}, 4000);
